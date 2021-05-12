3702e04c6c4d4de827d2ab4538e0e701df843396
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-05-12 15:08:51 +0200

    UPSTREAM: msm8953-common: Kill QTI gatekeeper HIDL
    - I have a strong belief this was dead since the beginning
    04-13 21:14:29.199   483   483 E vndksupport: Could not load /vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl-qti.so from default namespace: dlopen failed: library "libkeymasterdeviceutils.so" not found: needed by /system/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl-qti.so in namespace (default).
    - We're already shipping AOSP gatekeeper HIDL
    
    Change-Id: I0e77dee41d8811629a0ee968328f461a8bad9b69

--- msm8937-common-vendor-gatekeeper.mk
+++ msm8937-common-vendor-gatekeeper.mk
@@ -1,7 +1,3 @@
 PRODUCT_COPY_FILES += \
-    vendor/xiaomi/msm8937-common/gatekeeper/vendor/bin/hw/android.hardware.gatekeeper@1.0-service-qti:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.gatekeeper@1.0-service-qti \
-    vendor/xiaomi/msm8937-common/gatekeeper/vendor/etc/init/android.hardware.gatekeeper@1.0-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.gatekeeper@1.0-service-qti.rc \
-    vendor/xiaomi/msm8937-common/gatekeeper/vendor/lib/hw/android.hardware.gatekeeper@1.0-impl-qti.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/android.hardware.gatekeeper@1.0-impl-qti.so \
     vendor/xiaomi/msm8937-common/gatekeeper/vendor/lib/hw/gatekeeper.msm8937.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/gatekeeper.msm8937.so \
-    vendor/xiaomi/msm8937-common/gatekeeper/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl-qti.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/android.hardware.gatekeeper@1.0-impl-qti.so \
     vendor/xiaomi/msm8937-common/gatekeeper/vendor/lib64/hw/gatekeeper.msm8937.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/gatekeeper.msm8937.so
